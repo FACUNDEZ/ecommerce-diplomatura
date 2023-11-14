80171c9dcfc323cd505925a726d4e8ec6d756e6f
FACUNDEZ <facu.mdq@live.com>
2023-11-14 15:39:14 -0300

    Filtro by Fran 14//11

--- src/pages/store/index.tsx
+++ src/pages/store/index.tsx
@@ -1,20 +1,29 @@
-import { useState } from 'react'
-import Image from 'next/image';
-import { product } from '@/types/components.types';
+import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 
-function index() {
-    const router = useRouter()
-    const [products, setProducts] = useState([]);
+{/**Fran  */ }
+type Product = {
+    id: number;
+    title: string;
+    category: string;
+    price: number;
+    description: string;
+    image: string;
+};
+
+{/**Esto lo hice yo xd */ }
+function Index() {
+    const router = useRouter();
+    const [products, setProducts] = useState<Product[]>([]);
+    const [category, setCategory] = useState('');
 
     const getApi = async () => {
         try {
             const api = "https://fakestoreapi.com/products";
             const response = await fetch(api);
             const data = await response.json();
-
             setProducts(data);
-            console.log(data)
+            console.log(data);
         } catch (error) {
             console.log(error);
         }
@@ -24,11 +33,45 @@ function index() {
         getApi();
     }
 
+    {/**Filtro creado por Franco */ }
+    const handleCategoryChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setCategory(e.target.value);
+    };
+
+    const filteredProducts = category
+        ? products.filter((product) => product.category === category)
+        : products;
+
     return (
         <>
-        <button onClick={() => router.push("/")}>Volver al Inicio</button>
+            <button onClick={() => router.push("/")}>Volver al Inicio</button>
+
+            {/**Fran  */}
+
+            <div className='backgroundColor: red'>
+                <h1 className='text-xl font-bold text-black sm:text-3xl text-center max-w-md mx-auto mt-7'>Productos</h1>
+                <select value={category} onChange={handleCategoryChange}>
+                    <option value="">Todas las categorias</option>
+                    <option value="electronics">Electrónica</option>
+                    <option value="jewelery">Joyas</option>
+                    <option value="men's clothing">Ropa de Hombre</option>
+                    <option value="women's clothing">Ropa de Mujer</option>
+                </select>
+                <ul>
+                    {filteredProducts.map((product) => (
+                        <li key={product.id}>
+                            <h2>{product.title}</h2>
+                            <p>Categoría: {product.category}</p>
+                            <p>Precio: ${product.price}</p>
+                            <p>{product.description}</p>
+                            <img src={product.image} alt={product.title} width="200" />
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            {/**Yo */}
             <ul>
-                {products.map((product: product, index) => (
+                {products.map((product: Product, index) => (
                     <li key={index}>
                         <a href={`/store/products/${product.title}`} className="block group">
                             <img
@@ -46,7 +89,7 @@ function index() {
                 ))}
             </ul>
         </>
-    )
+    );
 }
 
-export default index
+export default Index;
