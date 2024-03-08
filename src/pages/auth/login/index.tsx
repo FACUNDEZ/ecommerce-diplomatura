4f0cb6e139dd96ff9a5f3a2358492d6b492fb6fb
FACUNDEZ <facu.mdq@live.com>
2024-03-08 15:30:04 -0300

    ewf

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -13,7 +13,7 @@ function Index() {
 
     const getApi = async () => {
         try {
-            const api = "http://localhost:3000/api/usuarios/login"
+            const api = "https://ecommerce-diplomatura-mobile.vercel.app//api/usuarios/login"
             const response = await fetch(api, {
                 method: 'POST',
                 headers: {
